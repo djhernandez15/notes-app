8f1e5d53f5921c0c0e3a6d82b9b9c23dfee471c4
David Hernandez Arias <david.hernandez3@coxautoinc.com>
2021-06-03 09:20:09 -0400

    adding notes through command line args

--- notes.js
+++ notes.js
@@ -1,5 +1,44 @@
+const fs = require("fs");
+const chalk = require("chalk");
 const getNotes = () => {
-	return "Your notes..."
-}
+	return "Your notes...";
+};
 
-module.exports = getNotes;
+const addNote = (title, body) => {
+	const notes = loadNotes();
+	const duplicateNotes = notes.filter((note) => {
+		return note.title === title;
+	});
+
+	if (duplicateNotes.length === 0) {
+		notes.push({
+			title,
+			body
+		});
+
+		saveNotes(notes);
+		console.log(chalk.green.italic("New note added!"));
+	} else {
+		console.log(chalk.red.italic("Note title taken!"));
+	}
+};
+
+const saveNotes = (notes) => {
+	const dataJSON = JSON.stringify(notes);
+	fs.writeFileSync("notes.json", dataJSON);
+};
+
+const loadNotes = () => {
+	try {
+		const dataBuffer = fs.readFileSync("notes.json");
+		const dataJSON = dataBuffer.toString();
+		return JSON.parse(dataJSON);
+	} catch (e) {
+		return [];
+	}
+};
+
+module.exports = {
+	getNotes: getNotes,
+	addNote: addNote
+};
