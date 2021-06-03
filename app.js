ef5b9ad3c692cdc579f4aed7ed1e7288009d5843
David Hernandez Arias <david.hernandez3@coxautoinc.com>
2021-06-03 07:54:29 -0400

    adding yargs

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const chalk = require('chalk');
 const getNotes = require('./notes');
 
-const notes = getNotes()
+const command = process.argv[2]
 
-console.log(notes)
-console.log(chalk.green.italic("Success!"))
+console.log(process.argv)
+
+if (command === 'add') {
+	console.log('Adding note!')
+} else if (command === 'remove') {
+	console.log('Removing note!')
+}
